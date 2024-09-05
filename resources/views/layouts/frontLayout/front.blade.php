ba9d42e3f3659f3db2da9fdad640a72b150b76b6
IvethGlez <128614203+ivethglez80@users.noreply.github.com>
2024-09-05 14:37:31 -0300

    added GTM script
    Added GTM script lines from 29 to 35 and 39 to 42

--- resources/views/layouts/frontLayout/front.blade.php
+++ resources/views/layouts/frontLayout/front.blade.php
@@ -26,10 +26,21 @@
   <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon_io/favicon-16x16.png') }}">
   <link rel="manifest" href="{{ asset('images/favicon_io/site.webmanifest') }}">
 
+   <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KD7TMV95');</script>
+<!-- End Google Tag Manager -->
+    
 </head>
 
-<body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KD7TMV95"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" aria-label="Offcanvas navbar large">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
